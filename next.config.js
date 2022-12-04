136de0648aa7932f0c75355092af01416b83d351
Marco Ramos <majramos@gmail.com>
2022-12-04 00:33:55 +0000

    fix static url

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
       unoptimized: true,
     },
+  assetPrefix: isProd ? '/<projectname>' : '',
 }
 
 module.exports = nextConfig
