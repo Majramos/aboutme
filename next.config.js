b71d05020ba065dfb0c05c17a17a29a346e0698b
Marco Ramos <majramos@gmail.com>
2022-12-04 01:15:14 +0000

    fix static url4

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   images: {
       unoptimized: true,
     },
-  assetPrefix: isProd ? '/aboutme/' : '',
+  //assetPrefix: isProd ? '/aboutme/' : '',
 }
 
 module.exports = nextConfig
