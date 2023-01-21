d091608c7053b1bdec778c9d38248bdd2b45b428
Marco Ramos <majramos@gmail.com>
2023-01-21 21:02:56 +0000

    refractor darkmode.js

--- public/scripts/darkmode.js
+++ public/scripts/darkmode.js
@@ -2,45 +2,45 @@
  * Script to handle the theme switching
  */
 
+const OtherTheme = { 'dark': 'light', 'light': 'dark' }
+
+function setTheme( theme ) {
+    var other_theme = OtherTheme[theme]
+
+    document.body.classList.remove('body-' + other_theme);
+    document.body.classList.add('body-' + theme);
+
+    localStorage.setItem('color-theme', theme);
+
+    document.getElementById('theme-toggle-' + theme + '-icon').style.display = 'inherit'; 
+    document.getElementById('theme-toggle-' + other_theme + '-icon').style.display = 'none'; 
+
+    document.getElementById('theme-text').innerText = 'Switch to ' + other_theme + ' mode';
+}
+
 function themeChecker() {
     // dark-mode media query matched or not
     if(localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-	    document.getElementById('theme-toggle-dark-icon').classList.remove('hidden');
-        document.body.classList.add('body-dark');
+        setTheme('dark');
     } else {
-        document.getElementById('theme-toggle-light-icon').classList.remove('hidden');
-        document.body.classList.add('body-light');
+        setTheme('light');
     }
 }
 
-function setDark() {
-    document.body.classList.replace('body-light', 'body-dark');
-    localStorage.setItem('color-theme', 'dark');
-}
-
-function setLight() {
-    document.body.classList.replace('body-dark', 'body-light');
-    localStorage.setItem('color-theme', 'light');
-}
-
 function changeTheme() {
-    // toggle icons inside button    
-    document.getElementById('theme-toggle-dark-icon').classList.toggle('hidden');
-    document.getElementById('theme-toggle-light-icon').classList.toggle('hidden');
-
     // if set via local storage previously
     if (localStorage.getItem('color-theme')) {
         if (localStorage.getItem('color-theme') === 'light') {
-            setDark()
+            setTheme('dark');
         } else {
-            setLight()
+            setTheme('light');
         }
     // if NOT set via local storage previously, keep as fallback option
     } else {
         if (document.body.classList.contains('body-dark')) {
-            setLight()
+            setTheme('light');
         } else {
-            setDark()
+            setTheme('dark');
         }
     }
 }
