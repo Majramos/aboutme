340e30b2a2b60cb9661b0dd2e2cc1b78d1585bae
Marco Ramos <majramos@gmail.com>
2024-08-01 10:47:03 +0100

    rename logos to socials

--- components/Socials.js
+++ components/Socials.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 
 
-function LogoLayout({ viewb = 512, children }) {
+function SvgTag({ viewb = 512, children }) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${viewb} 512`}
-            className="cursor-pointer h-6 md:h-8 w-6 md:w-8 transition duration-[250ms] hover:scale-[1.5]">
+            className="cursor-pointer h-6 sm:h-8 w-6 sm:w-8 hover:scale-[1.5]">
             {children}
         </svg>
     )
@@ -12,45 +12,45 @@ function LogoLayout({ viewb = 512, children }) {
 
 export function Linkedin() {
     return (
-        <LogoLayout viewb={448}>
+        <SvgTag viewb={448}>
             <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/>
-        </LogoLayout>
+        </SvgTag>
     )
 }
 
 export function Github() {
     return (
-        <LogoLayout viewb={496}>
+        <SvgTag viewb={496}>
             <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
-        </LogoLayout>
+        </SvgTag>
    )
 }
 
 export function Gitlab() {
     return (
-        <LogoLayout>
+        <SvgTag>
             <path d="M503.5 204.6L502.8 202.8L433.1 21.02C431.7 17.45 429.2 14.43 425.9 12.38C423.5 10.83 420.8 9.865 417.9 9.57C415 9.275 412.2 9.653 409.5 10.68C406.8 11.7 404.4 13.34 402.4 15.46C400.5 17.58 399.1 20.13 398.3 22.9L351.3 166.9H160.8L113.7 22.9C112.9 20.13 111.5 17.59 109.6 15.47C107.6 13.35 105.2 11.72 102.5 10.7C99.86 9.675 96.98 9.295 94.12 9.587C91.26 9.878 88.51 10.83 86.08 12.38C82.84 14.43 80.33 17.45 78.92 21.02L9.267 202.8L8.543 204.6C-1.484 230.8-2.72 259.6 5.023 286.6C12.77 313.5 29.07 337.3 51.47 354.2L51.74 354.4L52.33 354.8L158.3 434.3L210.9 474L242.9 498.2C246.6 500.1 251.2 502.5 255.9 502.5C260.6 502.5 265.2 500.1 268.9 498.2L300.9 474L353.5 434.3L460.2 354.4L460.5 354.1C482.9 337.2 499.2 313.5 506.1 286.6C514.7 259.6 513.5 230.8 503.5 204.6z"/>
-        </LogoLayout>
+        </SvgTag>
     )
 }
 
 export function Twitter() {
     return (
-        <LogoLayout viewb={448}>
+        <SvgTag viewb={448}>
             <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm297.1 84L257.3 234.6 379.4 396H283.8L209 298.1 123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5L313.6 116h47.5zM323.3 367.6L153.4 142.9H125.1L296.9 367.6h26.3z"/>
-        </LogoLayout>
+        </SvgTag>
     )
 }
 
 export function Email() {
     return (
-        <LogoLayout>
+        <SvgTag>
             <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/>
-        </LogoLayout>
+        </SvgTag>
     )
 }
 
-export default function HomeLogos() {
+export default function Socials() {
 
     useEffect(() => {
 
@@ -72,37 +72,34 @@ export default function HomeLogos() {
     }, []);
 
     return (
-        <div className="flex justify-center">
-            <div className="flex justify-center space-x-10 sm:space-x-20 py-6">
-                <a id="emaillink" href="mailto:marco@marcoramos.me" aria-label="link to email">
-					<Email />
-				</a>
-                <a id="linkedinlink" href="https://linkedin.com/in/majramos"
-					aria-label="link to linkedin"
-					target="_blank"
-					rel="noopener noreferrer">
-					<Linkedin />
-				</a>
-                <a id="gitlablink" href="https://gitlab.com/majramos"
-					aria-label="link to gitlab"
-					target="_blank"
-					rel="noopener noreferrer">
-					<Gitlab />
-				</a>
-                <a id="githublink" href="https://github.com/Majramos"
-					aria-label="link to github"
-					target="_blank"
-					rel="noopener noreferrer">
-					<Github />
-				</a>
-                <a id="twitterlink" href="https://x.com/majramos"
-					aria-label="link to x/twitter"
-					target="_blank"
-					rel="noopener noreferrer">
-					<Twitter />
-				</a>
-            </div>
-            <div id="linkshower" className="flex justify-center"><p></p></div>
-        </div>
+		<div className="flex justify-center space-x-10 sm:space-x-20">
+			<a id="emaillink" href="mailto:marco@marcoramos.me" aria-label="link to email">
+				<Email />
+			</a>
+			<a id="linkedinlink" href="https://linkedin.com/in/majramos"
+				aria-label="link to linkedin"
+				target="_blank"
+				rel="noopener noreferrer">
+				<Linkedin />
+			</a>
+			<a id="gitlablink" href="https://gitlab.com/majramos"
+				aria-label="link to gitlab"
+				target="_blank"
+				rel="noopener noreferrer">
+				<Gitlab />
+			</a>
+			<a id="githublink" href="https://github.com/Majramos"
+				aria-label="link to github"
+				target="_blank"
+				rel="noopener noreferrer">
+				<Github />
+			</a>
+			<a id="twitterlink" href="https://x.com/majramos"
+				aria-label="link to x/twitter"
+				target="_blank"
+				rel="noopener noreferrer">
+				<Twitter />
+			</a>
+		</div>
     )
 }
