940339f19292be341d167820f8f2117074524767
Marco Ramos <git@marcoramos.me>
2025-06-28 19:08:56 +0100

    apply formating to file

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,11 @@
-import Layout from '../components/Layout';
-
+import Layout from "../components/Layout";
 
 export default function Custom404() {
-    return (
-        <Layout title="Oops! 404">
-            <section className="relative flex min-h-screen flex-col justify-center text-center overflow-hidden">
-                <h1 className="text-xl sm:text-3xl font-bold">
-                    404 - Page Not Found
-                </h1>
-            </section>
-        </Layout>
-    )
+  return (
+    <Layout title="Oops! 404">
+      <section className="relative flex min-h-screen flex-col justify-center text-center overflow-hidden">
+        <h1 className="text-xl sm:text-3xl font-bold">404 - Page Not Found</h1>
+      </section>
+    </Layout>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,44 @@
-import Script from 'next/script';
-import { useEffect } from 'react';
+import Script from "next/script";
+import { useEffect } from "react";
 
-import '../styles/cookieconsent_custom.css';
-import '../styles/globals.css';
-
+import "../styles/cookieconsent_custom.css";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Script
-                type="module"
-                src="/scripts/cookieconsent-config.js"
-                strategy="beforeInteractive"
-            />
-            <Script
-                id="base-script"
-                src="/scripts/scripts.js"
-                strategy="beforeInteractive"
-                onReady={() => {
-                    themeChecker();
-                    DynamicNavBar();
-                }}
-            />
+  return (
+    <>
+      <Script
+        type="module"
+        src="/scripts/cookieconsent-config.js"
+        strategy="beforeInteractive"
+      />
+      <Script
+        id="base-script"
+        src="/scripts/scripts.js"
+        strategy="beforeInteractive"
+        onReady={() => {
+          themeChecker();
+          DynamicNavBar();
+        }}
+      />
 
-            <Component {...pageProps} />
+      <Component {...pageProps} />
 
-            <Script
-                type="module"
-                src="/scripts/particles-config.js"
-                strategy="afterInteractive"
-            />
-            {/*  Google Tag Manager */}
-            <Script
-                id="gtag-base"
-                strategy="afterInteractive"
-            >
-                {`
+      <Script
+        type="module"
+        src="/scripts/particles-config.js"
+        strategy="afterInteractive"
+      />
+      {/*  Google Tag Manager */}
+      <Script id="gtag-base" strategy="afterInteractive">
+        {`
                     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                     })(window,document,'script','dataLayer','GTM-5XGV4NT')
                 `}
-            </Script>
-        </>
-    )
+      </Script>
+    </>
+  );
 }
